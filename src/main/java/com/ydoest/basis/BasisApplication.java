39122e70e0448d6fce38acd35d0161a3e80b6e92
10755 <1075531212@qq.com>
2023-05-05 00:58:23 +0800

    1.注释掉jpa依赖 2.mongodb可以启动

--- src/main/java/com/ydoest/basis/BasisApplication.java
+++ src/main/java/com/ydoest/basis/BasisApplication.java
@@ -2,9 +2,8 @@ package com.ydoest.basis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@SpringBootApplication
 public class BasisApplication {
 
     public static void main(String[] args) {
