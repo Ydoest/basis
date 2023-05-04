e27543fe2542f6f5406f4c01e9d3e5e74d27ce2b
10755 <1075531212@qq.com>
2023-05-05 00:00:17 +0800

    1.尝试登陆mongo失败

--- src/main/java/com/ydoest/basis/BasisApplication.java
+++ src/main/java/com/ydoest/basis/BasisApplication.java
@@ -2,8 +2,9 @@ package com.ydoest.basis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class BasisApplication {
 
     public static void main(String[] args) {
